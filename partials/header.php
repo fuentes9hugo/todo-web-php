e79c369318737284157f0f068a97ae23e992e86d
Hugo Fuentes <fuentes9hugo@gmail.com>
2026-03-07 17:58:41 +0100

    Re-use HTML

---
+++ partials/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Bootstrap -->
+  <link
+   rel="stylesheet" 
+   href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.3.8/darkly/bootstrap.min.css" 
+   integrity="sha512-+uC0Ar9AG4/j/iF0Ug22TO9D17MAbD94K7J8h17EzXzN3D5kcOpYQdF4OuiLraHSibCVhz4DIcqwsDboRMVStg==" 
+   crossorigin="anonymous" 
+   referrerpolicy="no-referrer" 
+  />
+  <script
+   defer
+   src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" 
+   integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" 
+   crossorigin="anonymous"
+  ></script>
+
+  <!-- Static Content -->
+  <link rel="stylesheet" href="./static/css/index.css">
+
+<title>Contacts App</title>
+</head>
+<body>
+  <?php require "navbar.php" ?>
+
+  <main>
+
+  <!-- Content Here -->
